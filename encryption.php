8e8d0452fb6284fc577490f64550479de69fc773
batanete <bata619@gmail.com>
2014-12-17 01:53:19 +0000

    Update encryption.php

--- encryption.php
+++ encryption.php
@@ -5,12 +5,13 @@
     If successful,returns the sha1 hash of that password.Otherwise returns NULL.
     */
     function encryptPassword($intendedPass){
+        #verifies if password contains numbers and letters
         if (!preg_match('/[A-Za-z].*[0-9]|[0-9].*[A-Za-z]/', $intendedPass))
         {
             return NULL;
         }
-
-        if(strtolower($intendedPass)===$intendedPass)
+        #verifies if pass contains upper and lower characters
+        if((strtolower($intendedPass)===$intendedPass)||(strtoupper($intendedPass)===$intendedPass))
             return NULL;
 
         return sha1($intendedPass);
