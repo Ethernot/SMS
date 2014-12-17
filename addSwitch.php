ab8a435824231ec303ec01119d51aea198221c9a
Diogo <rxkax@hotmail.com>
2014-12-17 15:34:43 +0000

    add new Switch

---
+++ addSwitch.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title> Switch management Software </title>
+</head>
+<body>
+<h1>New Switch added with sucess</h1>
+<a href="Home.html">return to Home</a>
+<?php
+$brand = $_POST['brand'];
+$model = $_POST['model'];
+$ip = $_POST['ip'];
+$user = $_POST['user'];
+$password = $_POST['password'];
+$newSwitch = $brand . "," . $model . "," . $ip . "," . $user . "," . $password . "\n";
+if (!file_exists('Switch/SwitchAcessList.txt')) {
+    mkdir('Switch', 0777, true);
+}
+$myfile = fopen("Switch/SwitchAcessList.txt", "a") or die("Unable to open file!");
+fwrite($myfile, $newSwitch);
+fclose($myfile);
+?>
+</body>
+</html>
