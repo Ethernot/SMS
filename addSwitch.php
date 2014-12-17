01061eeabe5edf3d58eb702b843f12b0bf77a0bb
Diogo <rxkax@hotmail.com>
2014-12-17 15:40:45 +0000

    new switch ok

--- addSwitch.php
+++ addSwitch.php
@@ -5,12 +5,10 @@
     <title> Switch management Software </title>
 </head>
 <body>
-<h1>New Switch added with sucess</h1>
-<a href="Home.html">return to Home</a>
 <?php
 $brand = $_POST['brand'];
 $model = $_POST['model'];
-$ip = $_POST['ip'];
+$ip = $_POST['IP'];
 $user = $_POST['user'];
 $password = $_POST['password'];
 $newSwitch = $brand . "," . $model . "," . $ip . "," . $user . "," . $password . "\n";
@@ -21,5 +19,7 @@ $myfile = fopen("Switch/SwitchAcessList.txt", "a") or die("Unable to open file!"
 fwrite($myfile, $newSwitch);
 fclose($myfile);
 ?>
+<h1>New Switch added with sucess</h1>
+<a href="Home.html">return to Home</a>
 </body>
 </html>
